ed029a09a648d6b9ca47391b83656f23224493a8
Calvin <Calvin@Calvin_Laptop>
2015-01-17 22:16:32 -0800

    - Fixed "post" links to point to controller
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/views/news.php
+++ application/views/news.php
@@ -44,7 +44,7 @@
 					</div>
 					<h2>Updates: More Features Released <span>Brian Ferry</span></h2>
 					<p>
-						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="post.html" class="more">Read More</a></span>
+						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="/post" class="more">Read More</a></span>
 					</p>
 				</li>
 				<li>
@@ -56,7 +56,7 @@
 					</div>
 					<h2>Updates: More Features Released <span>Brian Ferry</span></h2>
 					<p>
-						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="post.html" class="more">Read More</a></span>
+						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="/post" class="more">Read More</a></span>
 					</p>
 				</li>
 				<li>
@@ -68,7 +68,7 @@
 					</div>
 					<h2>Updates: More Features Released <span>Brian Ferry</span></h2>
 					<p>
-						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="post.html" class="more">Read More</a></span>
+						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="/post" class="more">Read More</a></span>
 					</p>
 				</li>
 			</ul>
