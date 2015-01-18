594810d915142feca7ce2329d994bdefda874ce3
Calvin <Calvin@Calvin_Laptop>
2015-01-17 23:55:44 -0800

    - Removed commented-out menu code
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -13,23 +13,6 @@
 				<a href="/">Zero Type</a>
 			</div>
                         {menubar}
-			<!--ul id="navigation">
-				<li class="active">
-					<a href="/">Home</a>
-				</li>
-				<li>
-					<a href="/features">Features</a>
-				</li>
-				<li>
-					<a href="/news">News</a>
-				</li>
-				<li>
-					<a href="/about">About</a>
-				</li>
-				<li>
-					<a href="/contact">Contact</a>
-				</li>
-			</ul-->
 		</div>
 	</div>
         {content}
